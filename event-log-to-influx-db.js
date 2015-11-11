f2c43bb8f1e9f9c39c012e1673d95ca0e0df005a
Ryan Williams <ryan.blake.williams@gmail.com>
2015-11-11 17:49:52 +0000

    refactor event→influx script, sum over seconds

--- event-log-to-influx-db.js
+++ event-log-to-influx-db.js
@@ -1,58 +1,47 @@
 
-var filename = process.argv[2];
-
 var fs = require('fs');
+var argv = require('minimist')(process.argv.slice(2));
 var oboe = require('oboe');
 
+var filename = argv._[0];
 var is = fs.createReadStream(filename);
 
 var appId = null;
 var appName = null;
 
-var events = [];
-
 var stages = {};
+
+var seconds = [];
+
 var lastTime = null;
-
-function push(node, key, start, timeKey, tagsObj, fieldsObj) {
-  var event = {
-    key: key,
-    tags: tagsObj || {},
-    fields: fieldsObj || {}
-  };
-
-  event.tags.start = !!start;
-
+function getTime(node, timeKey) {
+  var curTime;
   if (timeKey instanceof Array) {
-    lastTime = timeKey.reduce(function(o, segment) {
+    curTime = timeKey.reduce(function(o, segment) {
       return (segment in o) ? o[segment] : null;
     }, node);
+  } else if (timeKey !== null) {
+    curTime = node[timeKey || 'Timestamp'];
+  }
+  if (curTime) {
+    curTime = parseInt(curTime / 1000);
+    lastTime = curTime;
   } else {
-    var curTime = node[timeKey || 'Timestamp'];
-    if (curTime) {
-      lastTime = curTime;
-    } else if (timeKey != null) {
+    curTime = lastTime;
+    if (timeKey !== null) {
       console.error("Missing time %s:", timeKey, node);
     }
   }
-  if (lastTime) {
-    event.timestamp = lastTime;
-  } else {
-    if (timeKey) {
-      console.error("No timestamp '%s' found for:", timeKey, node);
-    }
-  }
-
-  events.push(event);
-  return event;
+  return curTime;
 }
 
 function flattenObj(o, kvSep, fieldSep) {
   var r = [];
+  o.app = appId;
   for (var k in o) {
     var v = o[k];
     if (typeof v === 'string') {
-      v = v.replace(/ /g, '\\ ');
+      v = escape(v);
     }
     var a = [ k, v ];
     if (kvSep) {
@@ -66,83 +55,218 @@ function flattenObj(o, kvSep, fieldSep) {
   return r;
 }
 
+function escape(s) {
+  return s.toString().replace(/ /g, '\\ ');
+}
+
+
+function getSecond(t) {
+  if (!(t in seconds)) {
+    seconds[t] = {
+      taskStarts: 0,
+      taskEnds: {},
+      executorAdds: 0,
+      executorRemoves: {}
+    };
+  }
+  return seconds[t];
+}
+
+function tag(k, v) {
+  return k + '=' + escape(v);
+}
+
 console.log("Reading from:", filename);
+
+var numEvents = 0;
+var printInterval = setInterval(function() {
+  console.log("Processed %d events", numEvents);
+}, 1000);
+
 oboe(is).node('events[*]', function(node) {
-  if (node.Event == "SparkListenerStageSubmitted") {
+  numEvents++;
+  if (node.Event == "SparkListenerApplicationStart") {
+    appId = node["App ID"];
+    appId = appId.substring(appId.length - 4);
+    appName = node["App Name"];
+    var t = getTime(node);
+    var second = getSecond(t);
+    second.apps = second.apps || [];
+    second.apps.push({ id: appId, name: appName, start: true });
+  } else if (node.Event == "SparkListenerApplicationEnd") {
+    var t = getTime(node);
+    var second = getSecond(t);
+    second.apps = second.apps || [];
+    second.apps.push({ id: appId, name: appName, start: false });
+  } else if (node.Event == "SparkListenerJobStart") {
+    var t = getTime(node, 'Submission Time');
+    var second = getSecond(t);
+    second.jobs = second.jobs || [];
+    second.jobs.push({ id: node['Job ID'], start: true });
+  } else if (node.Event == "SparkListenerJobEnd") {
+    var t = getTime(node, 'Completion Time');
+    var second = getSecond(t);
+    second.jobs = second.jobs || [];
+    second.jobs.push({
+      id: node['Job ID'],
+      success: node['Job Result']['Result'] == 'JobSucceeded',
+      start: false
+    });
+  } else if (node.Event == "SparkListenerStageSubmitted") {
 
     var stageInfo = node['Stage Info'];
     var stageId = stageInfo['Stage ID'];
     var attemptId = stageInfo['Stage Attempt ID'];
 
-    var event = push(node, 'stage', true, null, { id: stageId + '.' + attemptId });
-    if (lastTime) {
-      event.timestamp = lastTime;
-    } else {
-      console.error("No time available for stage:", node);
-    }
-
     if (!(stageId in stages)) {
       stages[stageId] = {};
     }
-    stages[stageId][attemptId] = event;
+    stages[stageId][attemptId] = { id: stageId + '.' + attemptId, start: true, time: lastTime };
 
   } else if (node.Event == "SparkListenerStageCompleted") {
     var stageInfo = node['Stage Info'];
     var stageId = stageInfo['Stage ID'];
     var attemptId = stageInfo['Stage Attempt ID'];
 
-    push(node, 'stage', false, ['Stage Info', 'Completion Time'], { id: stageId + '.' + attemptId });
+    var t = getTime(node, ['Stage Info', 'Completion Time']);
+    var second = getSecond(t);
+    second.stages = second.stages || [];
+    second.stages.push({ id: stageId + '.' + attemptId, start: false });
 
-    var submissionTime = stageInfo['Submission Time'];
+    var submissionTime = getTime(node, ['Stage Info', 'Submission Time']);
     if (submissionTime) {
-      stages[stageInfo['Stage ID']][stageInfo['Stage Attempt ID']].timestamp = submissionTime;
+      var second = getSecond(submissionTime);
+      second.stages = second.stages || [];
+      var stage = stages[stageId][attemptId];
+      delete stage.time;
+      second.stages.push(stage);
+      delete stages[stageId][attemptId];
+      var isEmpty = true;
+      for (var k in stages[stageId]) {
+        isEmpty = false;
+        break;
+      }
+      if (isEmpty) {
+        delete stages[stageId];
+      }
     } else {
       console.error("Missing submission time on StageCompletion event:", node);
     }
 
-  } else if (node.Event == "SparkListenerJobStart") {
-    push(node, 'job', true, 'Submission Time');
-  } else if (node.Event == "SparkListenerJobEnd") {
-    push(node, 'job', false, 'Completion Time', { success: node['Job Result']['Result'] == 'JobSucceeded' });
   } else if (node.Event == "SparkListenerTaskStart") {
-    push(node, 'task', true, ['Task Info', 'Launch Time']);
+    var t = getTime(node, ['Task Info', 'Launch Time']);
+    var second = getSecond(t);
+    second.taskStarts++;
   } else if (node.Event == "SparkListenerTaskEnd") {
+
+    var t = getTime(node, ['Task Info', 'Finish Time']);
+    var second = getSecond(t);
+
     var reason = node["Task End Reason"]["Reason"];
     var success = !node['Task Info']['Failed'];
-    push(node, 'task', false, ['Task Info', 'Finish Time'], success ? { success: true } : { success: false, reason: reason });
-  } else if (node.Event == "SparkListenerApplicationStart") {
-    appId = node["App ID"];
-    appName = node["App Name"];
-    push(node, 'app', true, 'Timestamp', { name: appName });
-  } else if (node.Event == "SparkListenerApplicationEnd") {
-    push(node, 'app');
+
+    var key = tag('success', success);
+    if (!success) {
+      key += ',' + tag('reason', reason);
+    }
+    second.taskEnds[key] = (second.taskEnds[key] || 0) + 1;
+
   } else if (node.Event == "SparkListenerExecutorAdded") {
-    push(node, 'executor', true);
+    var t = getTime(node);
+    var second = getSecond(t);
+    second.executorAdds++;
   } else if (node.Event == "SparkListenerExecutorRemoved") {
-    push(node, 'executor', false, 'Timestamp', { reason: node['Removed Reason'] });
-  } else if (node.Event == "SparkListenerBlockManagerAdded") {
-    push(node, 'block_manager', true);
-  } else if (node.Event == "SparkListenerBlockManagerRemoved") {
-    push(node, 'block_manager');
-  }
-  if (events.length % 100000 == 0) {
-    console.log("Got %d events", events.length);
+    var t = getTime(node);
+    var second = getSecond(t);
+    var key = tag('reason', node['Removed Reason']);
+    second.executorRemoves[key] = (second.executorRemoves[key] || 0) + 1;
   }
 }).done(function(o) {
-  var outFilename = process.argv.length >= 4 ? process.argv[3] : filename.replace(/\.json$/, ".idb");
-  console.log("Got %d events; writing to file: ", events.length, outFilename);
+  clearInterval(printInterval);
+
+  for (var stageId in stages) {
+    for (var attemptId in stages[stageId]) {
+      var stage = stages[stageId][attemptId];
+      console.log("Unfinished stage:", stage);
+      var second = getSecond(stage.time);
+      delete stage.time;
+      second.stages = second.stages || [];
+      second.stages.push(stage);
+    }
+  }
+
+  var outFilename = argv._.length >= 2 ? argv._[1] : filename.replace(/\.json$/, ".idb");
+  console.log("Got %d events; writing to file: ", numEvents, outFilename);
   if (!appId) {
     console.error("App ID not found!");
   }
+
+  var numSeconds = 0;
+  var secondsPrintInterval = setInterval(function() {
+    console.log("Processed %d seconds", numSeconds);
+  }, 1000);
+
   var fd = fs.openSync(outFilename, 'w');
-  fs.writeSync(fd, events.map(function(event) {
-    event.tags.app = appId;
-    return [
-      [ event.key, flattenObj(event.tags, '=', ',')].join(','),
-      'value=1',
-      event.timestamp + '000000'
-    ].join(' ')
-  }).join('\n'));
-}).fail(function(thrown, statusCode, body, jsonBody) {
-  console.error("Fail:", arguments);
+  var stageTasksRunning = 0;
+
+  for (var s in seconds) {
+    function writeCount(k, key) {
+      if (second[k]) {
+        fs.writeSync(
+              fd,
+              [
+                [key, flattenObj({}, '=', ',')].join(','),
+                'value=' + second[k],
+                s + '000000000'
+              ].join(' ') + '\n'
+        );
+      }
+    }
+
+    function writeObj(k) {
+      (second[k + 's'] || []).forEach(function(o) {
+        fs.writeSync(
+              fd,
+              [
+                [ k, flattenObj(o, '=', ',')].join(','),
+                'value=1',
+                s + '000000000'
+              ].join(' ') + '\n'
+        );
+      });
+    }
+
+    function writeCounts(secondKey, influxKey) {
+      var obj = second[secondKey];
+      for (var k in obj) {
+        var v = obj[k];
+        if (v) {
+          fs.writeSync(
+                fd,
+                [
+                  [influxKey, k, flattenObj({}, '=', ',')].join(','),
+                  'value=' + v,
+                  s + '000000000'
+                ].join(' ') + '\n'
+          );
+        }
+      }
+    }
+
+    numSeconds++;
+    var second = seconds[s];
+    second.app = appId;
+    writeObj('app');
+    writeObj('job');
+    writeObj('stage');
+
+    writeCount('taskStarts', 'ts');
+    writeCounts('taskEnds', 'te');
+    writeCount('executorAdds', 'es');
+    writeCounts('executorRemoves', 'ee');
+  }
+  console.log("done");
+  clearInterval(secondsPrintInterval);
+}).fail(function(e) {
+  console.error("Oboe caught error:", e.thrown.stack);
 });
